41c9738c887e703036565a64fddeabca4340c32e
CGAnVil <hoangminh.tlu@gmail.com>
2022-01-06 14:52:30 +0700

    hoan thien bai tap

--- src/com/codegym/Main.java
+++ src/com/codegym/Main.java
@@ -1,17 +1,35 @@
 package com.codegym;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int[][] array = {
-                {1, 2, 3, 4},
-                {5, 60, 2, 1},
-                {3, 9, 8, 7},
-        };
+//        int[][] array = {
+//                {1, 2, 3, 4},
+//                {5, 60, 2, 1},
+//                {3, 9, 8, 7},
+//        };
+//        int max = findMax(array);
+//        System.out.println(max);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter rows:");
+        int row = sc.nextInt();
+        System.out.println("Enter colum:");
+        int colum = sc.nextInt();
+        int[][] array = new int[row][colum];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < colum; j++) {
+                System.out.println("Enter element in row: " + (i + 1) + " colum:" + (j + 1));
+                array[i][j] = sc.nextInt();
+            }
+
+        }
         int max = findMax(array);
-        System.out.println(max);
+        System.out.println("Max value in array: " + max);
     }
-    public static int findMax (int[][] array){
+
+    public static int findMax(int[][] array) {
         int max = array[0][0];
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
